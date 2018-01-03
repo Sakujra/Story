4dca0fad1c039c7fb67da9f2bf2265def0a1ea65
DengCun <972493523@qq.com>
2018-01-03 16:52:45 +0800

    收藏列表

---
+++ app/src/main/java/com/vincentlaf/story/adapter/CollectionListAdapter.java
@@ -0,0 +1,10 @@
+package com.vincentlaf.story.adapter;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+
+/**
+ * Created by DengCun on 2018/1/3.
+ */
+
+public class CollectionListAdapter extends BaseQuickAdapter {
+}
---
+++ app/src/main/java/com/vincentlaf/story/bean/ItemCollection.java
@@ -0,0 +1,8 @@
+package com.vincentlaf.story.bean;
+
+/**
+ * Created by DengCun on 2018/1/3.
+ */
+
+public class ItemCollection {
+}
