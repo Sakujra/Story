34ffdb8fb140925167e24ed41f464432d6be29a8
记忆流年 <1486609892@qq.com>
2018-01-05 10:38:28 +0800

    添加经纬度

--- app/src/main/java/com/vincentlaf/story/bean/netbean/StoryListInfo.java
+++ app/src/main/java/com/vincentlaf/story/bean/netbean/StoryListInfo.java
@@ -16,6 +16,24 @@ public class StoryListInfo {
     private String userPic;
     private Double distance;
     private Date date;
+    private Double lon;
+    private Double lat;
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
 
     public String getUserName() {
         return userName;
